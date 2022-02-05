342fdae8a85a3b24706ef75225ef99ed1fe61dba
Joakim Stien <joakim.stien@icloud.com>
2022-02-05 14:10:14 +0100

    MVP of Apartment

---
+++ Assets/StaticCollder.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticCollder: MonoBehaviour
+{
+    // Use this for initialization
+    void Start () {
+        foreach (Transform childObject in transform) {
+            Mesh mesh = childObject.gameObject.GetComponent<MeshFilter>().mesh;
+            if (mesh != null) {
+                MeshCollider meshCollider = childObject.gameObject.AddComponent<MeshCollider>();
+                meshCollider.sharedMesh = mesh;
+            }
+        }
+    }
+}
