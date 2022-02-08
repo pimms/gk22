8a921d77da269772ce6d4849b189918f5a125189
Joakim Stien <joakim.stien@icloud.com>
2022-02-08 15:00:06 +0100

    Placeable, fixed oopsie

---
+++ Assets/Scripts/PlaceableObject.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum PlaceableItemType {
+    Toothrush
+}
+
+class PlaceableObject: MonoBehaviour {
+    public PlaceableItemType type;
+}
---
+++ Assets/Scripts/PlaceableSpot.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum PlaceableSpotType {
+    Toilet
+}
+
+class PlaceableSpot: MonoBehaviour {
+    public PlaceableSpotType type;
+}
